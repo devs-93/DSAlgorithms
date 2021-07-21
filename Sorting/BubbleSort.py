782729a248e52700d7a62f62c23197ff8f690cee
devs-93 <shukla.devbrat1993@gmail.com>
2021-07-21 13:09:36 +0530

    Bubble sort algo

---
+++ Sorting/BubbleSort.py
@@ -0,0 +1,13 @@
+def bubble_sort(A):
+    n = len(A)
+    for j in range(0, n - 1):
+        for i in range(0, n - 1 - j):
+            if A[i] > A[i + 1]:
+                temp = A[i]
+                A[i] = A[i + 1]
+                A[i + 1] = temp
+
+
+data_list = eval(input())
+bubble_sort(data_list)
+print(data_list)
