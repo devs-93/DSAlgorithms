4d66a43634ed7b2f51e52321998c29b9921d1d7a
devs-93 <shukla.devbrat1993@gmail.com>
2021-07-22 15:41:43 +0530

    Bubble sort has a worst-case and average complexity of О(n2)

--- Sorting/BubbleSort.py
+++ Sorting/BubbleSort.py
@@ -1,6 +1,7 @@
 def bubble_sort(A):
     n = len(A)
     for j in range(0, n - 1):
+        #reducing the no of compairsion while sorting
         for i in range(0, n - 1 - j):
             if A[i] > A[i + 1]:
                 temp = A[i]
