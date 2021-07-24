8189b349a3cd52c6361087b4db9879e34db79a0a
devs-93 <shukla.devbrat1993@gmail.com>
2021-07-24 12:27:53 +0530

    LinkedList basic operation using python operations are given below (1) add element (2) size of linked list (3) display element

--- LinkedList/LinkedList.py
+++ LinkedList/LinkedList.py
@@ -27,11 +27,46 @@ class LinkedList:
         self._tail = new_element
         self._size = self._size + 1
 
+    def add_at_the_beginning(self, e):
+        new_element = _Node(e, None)
+        if self.is_empty():
+            self._head = new_element
+            self._tail = new_element
+        else:
+            new_element._next = self._head
+            self._head = new_element
+        self._size = self._size + 1
+
+    def add_at_any_position(self, e, pos):
+        new_element = _Node(e, None)
+        p = self._head
+        index = 1
+        if self._size < pos or pos < index:
+            return -1
+        while index < pos-1:
+            p = p._next
+            index = index + 1
+        temp = p._next
+        p._next = new_element
+        new_element._next = temp
+        self._size = self._size + 1
+
     def display(self):
         p = self._head
         while p:
             print(p._element, end=" ")
             p = p._next
+
+    def search(self, key):
+        p = self._head
+        index = 0
+        while p != None:
+            if p._element == key:
+                return index
+            else:
+                p = p._next
+                index = index + 1
+        return -1
 
 
 n = LinkedList()
@@ -41,4 +76,19 @@ n.add_at_the_last(30)
 n.add_at_the_last(40)
 n.add_at_the_last(50)
 print(len(n))
+print("Linked List")
 n.display()
+print()
+print("Add at the beginning :")
+n.add_at_the_beginning(100)
+n.display()
+print()
+print(len(n))
+print("Insert at Given Position")
+print("insert element and position :")
+e, p = input().split(" ")
+n.add_at_any_position(int(e), int(p))
+print("display after insertion :")
+n.display()
+# key = int(input("enter key to search into the lined list: "))
+# print("Result :", n.search(key))
