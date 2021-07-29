3717628863e39944a99fe4b7eaf7107934c1e351
devs-93 <shukla.devbrat1993@gmail.com>
2021-07-29 16:46:37 +0530

    Creation of Binary Tress using Class tree traversal (1) preorder traversal (2) postorder traversal (3) inorder traversal (4) level order traversal (5) node count in binary tree

--- Trees/BinaryTree.py
+++ Trees/BinaryTree.py
@@ -35,24 +35,26 @@ class BinaryTree:
             print(t_root._element, end=" ")
             self.preorder_traversal(t_root._right)
 
-    def level_order(self,t_root):
+    def level_order(self, t_root):
         Q = QueueLinkedList()
         t = t_root
         print(t._element, end=" ")
         Q.enqueue(t)
         while not Q.isempty():
-            removed_address=Q.dequeue()
+            removed_address = Q.dequeue()
             if removed_address._left:
-                print(removed_address._left._element,end=" ")
+                print(removed_address._left._element, end=" ")
                 Q.enqueue(removed_address._left)
             if removed_address._right:
-                print(removed_address._right._element,end=" ")
+                print(removed_address._right._element, end=" ")
                 Q.enqueue(removed_address._right)
 
-
-
-
-
+    def node_count(self, t_root):
+        if t_root:
+            x = self.node_count(t_root._left)
+            y = self.node_count(t_root._right)
+            return (x + y + 1)
+        return 0
 
 
 x = BinaryTree()
@@ -63,14 +65,18 @@ x.make_tree(20, a, a)
 y.make_tree(30, a, a)
 z.make_tree(10, x, y)
 
-print("preorder_traversal")
+print("preorder_traversal :")
 x.preorder_traversal(z._root)
 print()
-print("postorder_traversal")
+print("postorder_traversal :")
 x.postorder_traversal(z._root)
 print()
-print("inorder_traversal")
+print("inorder_traversal :")
 x.inorder_traversal(z._root)
 print()
-print("level_order")
+print("level_order :")
 x.level_order(z._root)
+
+print()
+print("node count in the tree :")
+print(x.node_count(z._root))
