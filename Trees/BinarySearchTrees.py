91a11e95f452af739730038520948e2e3350b7be
devs-93 <shukla.devbrat1993@gmail.com>
2021-07-30 15:52:40 +0530

    Creation of Binary Search Tress using Class and recursive method

--- Trees/BinarySearchTrees.py
+++ Trees/BinarySearchTrees.py
@@ -11,7 +11,7 @@ class BinarySearchTree:
     def __init__(self):
         self._root = None
 
-    def insert(self, t_root, e):
+    def insert_iter(self, t_root, e):
         temp = None
         while t_root:
             temp = t_root
@@ -30,6 +30,17 @@ class BinarySearchTree:
             elif temp._element < e:
                 temp._right = new_node
 
+    def insert_recur(self, t_root, e):
+        if t_root:
+            if e < t_root._element:
+                t_root._left = self.insert_recur(t_root._left, e)
+            elif e > t_root._element:
+                t_root._right = self.insert_recur(t_root._right, e)
+        else:
+            n = _Node(e, None, None)
+            t_root = n
+        return t_root
+
     def inorder_traversal(self, t_root):
         if t_root:
             self.inorder_traversal(t_root._left)
@@ -37,21 +48,26 @@ class BinarySearchTree:
             self.inorder_traversal(t_root._right)
 
 
+
+##Binary Search Tree using iterative approach
 b = BinarySearchTree()
-b.insert(b._root, 100)
-b.insert(b._root, 30)
-b.insert(b._root, 40)
-b.insert(b._root, 50)
-b.insert(b._root, 20)
-b.insert(b._root, 60)
-b.insert(b._root, 200)
-b.insert(b._root, 300)
-b.insert(b._root, 30)
-b.insert(b._root, 40)
-b.insert(b._root, 50)
-b.insert(b._root, 400)
-b.insert(b._root, 500)
-b.insert(b._root, 600)
-
+b.insert_iter(b._root, 100)
+b.insert_iter(b._root, 30)
+b.insert_iter(b._root, 40)
+b.insert_iter(b._root, 50)
+b.insert_iter(b._root, 20)
+b.insert_iter(b._root, 60)
 b.inorder_traversal(b._root)
+print()
 
+##Binary Search Tree using recursive approach
+c = BinarySearchTree()
+c._root = c.insert_recur(c._root, 100)
+c.insert_recur(c._root, 2)
+c.insert_recur(c._root, 3)
+c.insert_recur(c._root, 4)
+c.insert_recur(c._root, 5)
+c.insert_recur(c._root, 6)
+c.insert_recur(c._root, 7)
+c.insert_recur(c._root, 8)
+c.inorder_traversal(c._root)
